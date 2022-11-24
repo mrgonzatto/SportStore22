bef3952929185dc11c713a3efedd3d510af9f295
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-11-23 22:07:55 -0300

    23-11-22

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            return View(
+                repository.Products
+                .Select(p => p.Category)
+                .Distinct()        
+                .OrderBy( p => p )
+            );
+        }
+    }
+}
